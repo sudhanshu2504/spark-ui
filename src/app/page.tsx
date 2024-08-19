9a6fe4f402323350bc3ccdc4db3995268f225eef
Sudhanshu-WMW <sudhanshu250403@gmail.com>
2024-08-19 14:00:38 +0530

    SEO optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,9 +27,9 @@ const Content = () => {
         >
           <span className="bg-clip-text z-0 bg-gradient-to-br to-[rgb(255,255,255,0.18)] from-[rgb(255,255,255,0.38)]">Introducing SparkUI</span>
         </HoverBorderGradient>
-        <div className="font-black md:text-4xl text-base text-center text-white text-wrap max-w-screen-md px-4">
+        <h1 className="font-black md:text-4xl text-base text-center text-white text-wrap max-w-screen-md px-4">
         Transform your web projects into <span className="text-yellow-400 underline underline-offset-2">Spark</span>, the next-generation <span className="text-yellow-400 underline underline-offset-2">UI Library</span> designed for seamless, dynamic, and stunning user interfaces.
-        </div>
+        </h1>
       </div>
       <div className="flex flex-col lg:my-10 my-4 gap-y-4">
         <div className="flex flex-row flex-wrap justify-center items-center gap-2 ">
@@ -43,15 +43,15 @@ const Content = () => {
         <div className="flex flex-row justify-center flex-wrap gap-2 my-2 lg:my-10">
           <div className="flex items-center gap-1">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="md:h-10 md:w-10 h-4 w-4 text-neutral-500 flex-shrink-0 stroke-1"><path d="M6.306 8.711c-2.602 .723 -4.306 1.926 -4.306 3.289c0 2.21 4.477 4 10 4c.773 0 1.526 -.035 2.248 -.102"></path><path d="M17.692 15.289c2.603 -.722 4.308 -1.926 4.308 -3.289c0 -2.21 -4.477 -4 -10 -4c-.773 0 -1.526 .035 -2.25 .102"></path><path d="M6.305 15.287c-.676 2.615 -.485 4.693 .695 5.373c1.913 1.105 5.703 -1.877 8.464 -6.66c.387 -.67 .733 -1.339 1.036 -2"></path><path d="M17.694 8.716c.677 -2.616 .487 -4.696 -.694 -5.376c-1.913 -1.105 -5.703 1.877 -8.464 6.66c-.387 .67 -.733 1.34 -1.037 2"></path><path d="M12 5.424c-1.925 -1.892 -3.82 -2.766 -5 -2.084c-1.913 1.104 -1.226 5.877 1.536 10.66c.386 .67 .793 1.304 1.212 1.896"></path><path d="M12 18.574c1.926 1.893 3.821 2.768 5 2.086c1.913 -1.104 1.226 -5.877 -1.536 -10.66c-.375 -.65 -.78 -1.283 -1.212 -1.897"></path><path d="M11.5 12.866a1 1 0 1 0 1 -1.732a1 1 0 0 0 -1 1.732z"></path></svg>
-              <span className="font-bold text-gray-400">React JS</span>
+              <h3 className="font-bold text-gray-400">React JS</h3>
           </div>
           <div className="flex items-center gap-2">
           <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" className="md:h-10 md:w-10 h-4 w-4 text-neutral-500 flex-shrink-0 stroke-[0.5px]" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M18.5 9.51a4.22 4.22 0 0 1-1.91-1.34A5.77 5.77 0 0 0 12 6a4.72 4.72 0 0 0-5 4 3.23 3.23 0 0 1 3.5-1.49 4.32 4.32 0 0 1 1.91 1.35A5.77 5.77 0 0 0 17 12a4.72 4.72 0 0 0 5-4 3.2 3.2 0 0 1-3.5 1.51zm-13 4.98a4.22 4.22 0 0 1 1.91 1.34A5.77 5.77 0 0 0 12 18a4.72 4.72 0 0 0 5-4 3.23 3.23 0 0 1-3.5 1.49 4.32 4.32 0 0 1-1.91-1.35A5.8 5.8 0 0 0 7 12a4.72 4.72 0 0 0-5 4 3.2 3.2 0 0 1 3.5-1.51z"></path></svg>
-              <span className="font-bold text-gray-400">Tailwind CSS</span>
+              <h3 className="font-bold text-gray-400">Tailwind CSS</h3>
           </div>
           <div className="flex items-center gap-2">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="md:h-10 md:w-10 h-4 w-4 text-neutral-500 flex-shrink-0 stroke-1"><path d="M12 12l-8 -8v16l16 -16v16l-4 -4"></path><path d="M20 12l-8 8l-4 -4"></path></svg>
-              <span className="font-bold text-gray-400">Framer Motion</span>
+              <h3 className="font-bold text-gray-400">Framer Motion</h3>
           </div>
         </div>
       </div>
