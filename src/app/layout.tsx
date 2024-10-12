b340900ff250c6dae011244cc70291d290e4f21a
sudhanshu2504 <sudhanshu250403@gmail.com>
2024-10-13 00:08:01 +0530

    Analytics enabled

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import {NextUIProvider} from "@nextui-org/react";
 import Navigation from "../components/navbar";
 import Footer from "../components/footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "SparkUI | Frontend Library",
@@ -45,6 +46,7 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
   return (
     <html lang="en" className="bg-black">
       <body>
+        <Analytics/>
         <NextUIProvider>
           <Navigation/>
           <div className="min-h-screen">
