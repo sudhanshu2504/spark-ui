2faad090b0d011ffb2359f6978b2bd9830aaf213
sudhanshu2504 <sudhanshu250403@gmail.com>
2025-11-23 17:18:22 +0530

    fix

--- src/content/component_mapper.js
+++ src/content/component_mapper.js
@@ -9,11 +9,6 @@ import MagneticEffects from './demo/MagneticEffects';
 import MouseFollower from './demo/MouseFollower';
 import TextGradientOnhover from './demo/TextGradientOnhover';
 import DefaultComponent from './demo/DefaultComponent';
-import BentoGrid from './demo/BentoGrid';
-import FloatingDock from './demo/FloatingDock';
-import AuroraBackground from './demo/AuroraBackground';
-import ThreeDTiltCard from './demo/ThreeDTiltCard';
-import TypewriterEffect from './demo/TypewriterEffect';
 
 const componentMapper = {
     'animated-carousel': Carousel,
@@ -25,12 +20,7 @@ const componentMapper = {
     'image-slider': ImageSliders,
     'magnetic-effect': MagneticEffects,
     'mouse-follower': MouseFollower,
-    'text-gradient-onhover': TextGradientOnhover,
-    'bento-grid': BentoGrid,
-    'floating-dock': FloatingDock,
-    'aurora-background': AuroraBackground,
-    '3d-tilt-card': ThreeDTiltCard,
-    'typewriter-effect': TypewriterEffect,
+    'text-gradient-onhover': TextGradientOnhover
 }
 
 export default componentMapper;
